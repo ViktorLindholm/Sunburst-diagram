104f6f1f9765c04c7b84e13876e4ea3422b07afe
v <v_lindholm@hotmail.com>
2019-04-25 13:15:58 +0200

    testindex

--- 2steprender/zoomable-sunburst.js
+++ 2steprender/zoomable-sunburst.js
@@ -39,7 +39,6 @@ const v1 = {
       .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.code); })
       .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
       .attr("d", d => arc(d.current));
-
       path.filter(d => d.children)
       .style("cursor", "pointer")
       .on("click", clicked);
